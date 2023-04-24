c8f2261bcbd331fa57515944a43450c0a3fa6794
sammy-code98 <sammysmart98@yahoo.com>
2023-04-24 22:21:54 +0100

    feat:auth page #1

--- src/Modules/ForgotPassword/index.tsx
+++ src/Modules/ForgotPassword/index.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { Box, Text, Image, InputGroup, InputLeftElement, Input, Button } from "@chakra-ui/react";
 import forgotPassImg from "../../assets/forgot-password.svg"
 import { MdOutlineEmail } from "react-icons/md";
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import { forgotPasswordSchema } from "@/yupSchema";
+
+interface ForgotPasswordInput {
+  email: string
+}
 
 
 function ForgotPassword(): JSX.Element {
+  const { register, handleSubmit, formState: { errors } } = useForm<ForgotPasswordInput>({
+    resolver: yupResolver(forgotPasswordSchema)
+  })
+
+  const onSubmit = (data: ForgotPasswordInput) => console.log(data)
+
   return (
     <Box
       display='flex'
@@ -27,18 +40,24 @@ function ForgotPassword(): JSX.Element {
         <Text fontSize='3xl' textAlign='center' fontWeight='bold'>Forgot  password?</Text>
         <Text textAlign='center' textColor='#606172' mt='1rem'>Enter your email address and we’ll send you instructions on how to reset your password.</Text>
 
-        <Box as='form'>
+        <Box as='form' onSubmit={handleSubmit(onSubmit)}>
           <Box py='1rem'>
             <InputGroup>
               <InputLeftElement
                 pointerEvents='none'
                 children={<MdOutlineEmail color='#979797' />}
               />
-              <Input type='email' placeholder='email@gmail.com' variant='filled' />
+              <Input
+                type='email'
+                placeholder='email@gmail.com'
+                variant='filled'
+                {...register("email")}
+              />
             </InputGroup>
+            <Text as='span' color='error'>{errors.email?.message}</Text>
           </Box>
           <Box mt='1rem' display='flex' justifyContent='center'>
-            <Button size='md' variant='secondary' width='100%' >Log In</Button>
+            <Button size='md' variant='secondary' width='100%' type="submit" >Log In</Button>
           </Box>
         </Box>
       </Box>
